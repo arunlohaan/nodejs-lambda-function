dff9e49b32f3c64e867f091c7955213a52f74f41
arunlohaan <alohaan@ekcs.co>
2023-08-02 18:10:28 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("My First Lambda Function"),
+    body: JSON.stringify("My First update to Lambda Function"),
   }
   return response
 }
